44a85101d6f694afde5025285751c6041c782231
Bobby Kumar <kashyapashish0002@gmail.com>
2022-01-26 22:37:50 +0530

    Added nav bar and idea card

--- src/Components/dumpwall/IdeaCard.jsx
+++ src/Components/dumpwall/IdeaCard.jsx
@@ -1,10 +1,31 @@
-import React from "react";
+import React, { useContext, useState } from "react";
 import "./dumpwall.css";
 
-const IdeaCard = () => {
+import vote from "../../../images/vote.svg";
+
+const IdeaCard = (props) => {
   return (
     <>
-      <div className="idea_card"></div>
+      <section className="idea__card">
+        <h1 className="heading">Trending Idea..</h1>
+        <div className="card__container">
+          <span className="card__img__container"></span>
+          <div className="idea__description__container">
+            <h3 className="idea__name">Your Idea Name</h3>
+            <p className="idea__description">Complete discription of the product Complete discription of the product Complete discription of the product</p>
+            <span className="links__container">
+              <p className="comment__link">comment</p>
+              <p className="share__link">share</p>
+            </span>
+          </div>
+          <span className="vote__container">
+            <img src={vote} alt="" className="vote__img" />
+            {/* <p className="vote__count">
+              {upVote}
+            </p> */}
+          </span>
+        </div>
+      </section>
     </>
   );
 };
--- src/Components/dumpwall/Navbar.jsx
+++ src/Components/dumpwall/Navbar.jsx
@@ -1,10 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import "./dumpwall.css";
 
+import logo from "../../../images/logo.svg";
+
 const Navbar = () => {
+  const [burgerStatus, setBurgerStatus] = useState ("close");
   return (
     <>
-      <div className="navbar"></div>
+      <nav class="nav__bar">
+        <div className="navigation">
+          <span class="logo">
+            <img src={logo} title="Dezenix" alt="Dezenix logo" class="logo__img" />
+          </span>
+          <div class="navbar__buttons">
+              <button class="navigation__buttons">home</button>
+              <button class="navigation__buttons">about us</button>
+              <button id="submit__idea" class="navigation__buttons">submit idea</button>
+          </div>
+        </div>
+        <div className="mobile__navigation">
+          <div role="button" onClick={() => setBurgerStatus((burgerStatus === "open") ? "close" : "open")} className="hamburger__menu">
+            <i className={burgerStatus}></i>
+            <i className={burgerStatus}></i>
+            <i className={burgerStatus}></i>
+          </div>
+          <span class="logo">
+            <img src={logo} title="Dezenix" alt="Dezenix logo" class="logo__img" />
+          </span>
+          <button id="submit__idea" class="navigation__buttons">submit idea</button>
+        </div>
+    </nav>
+    <div id={burgerStatus}>
+      <button id="burger__buttons" class="navigation__buttons" onClick={() => setBurgerStatus((burgerStatus === "open") ? "close" : "open")}>home</button>
+      <button id="burger__buttons" class="navigation__buttons" onClick={() => setBurgerStatus((burgerStatus === "open") ? "close" : "open")}>about us</button>
+    </div>
     </>
   );
 };
