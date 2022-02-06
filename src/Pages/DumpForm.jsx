460c0f31b4515f3fda3830948569fe8ac3a82354
Sahil Gupte <sahilsachingupte@gmail.com>
2022-02-06 23:11:20 +0530

    Show loading while form is submitting
    Signed-off-by: Sahil Gupte <sahilsachingupte@gmail.com>

--- src/Pages/DumpForm.jsx
+++ src/Pages/DumpForm.jsx
@@ -10,6 +10,7 @@ let fileToBeUploaded;
 
 const DumpForm = () => {
   const navigate = useNavigate();
+  const [loading, setLoading] = useState(false);
   const [formData, setformData] = useState({
     name: '',
     email: '',
@@ -26,6 +27,9 @@ const DumpForm = () => {
 
   const submitIdea = async (e) => {
     e.preventDefault();
+
+    setLoading(true);
+
     const { name, email, description, imageURL } = formData;
 
     if (name && email && description) {
@@ -46,6 +50,7 @@ const DumpForm = () => {
       }
 
       setformData({ name: '', email: '', description: '' });
+
       alert('Idea Submitted');
       navigate('/');
     }
@@ -61,91 +66,100 @@ const DumpForm = () => {
   return (
     <>
       <div className="dump_form">
-        <Link to="/">
-          <button className="cancel__form">X</button>
-        </Link>
-        <div className="form__heading">
-          <h1 id="greeting__msg">Hi There &#128075;</h1>
-          <p id="form__msg">
-            Feel free to share your ideas and the community will help you to turn it into a product.
-          </p>
-        </div>
-        <form className="form__container">
-          <label htmlFor="name" className="input__lables">
-            Enter your full name:<span className="asteriks">*</span>
-          </label>
-          <input
-            className="input__field"
-            type="text"
-            id="name"
-            name="name"
-            value={formData.name}
-            onChange={handleChange}
-            required
-            autoComplete="off"
-          />
-          <label htmlFor="email" className="input__lables">
-            Enter email address:<span className="asteriks">*</span>
-          </label>
-          <input
-            className="input__field"
-            type="email"
-            id="email"
-            name="email"
-            value={formData.email}
-            onChange={handleChange}
-            required
-            autoComplete="off"
-          />
-          <label htmlFor="description" className="input__lables">
-            Brief Description of idea:<span className="asteriks">*</span>
-          </label>
-          <textarea
-            className="input__field text__area"
-            type="text"
-            id="description"
-            name="description"
-            value={formData.description}
-            onChange={handleChange}
-            required
-            autoComplete="off"
-          />
-          <label htmlFor="dropzone" className="input__lables">
-            Please share Graphical Description (If any)
-          </label>
-          <div
-            draggable
-            onDragOver={(e) => {
-              e.preventDefault();
-              e.stopPropagation();
-            }}
-            onDrop={(event) => onImageDropped(event)}
-            className="input__field drop__zone"
-            title="Drop Your Graphical Description Here"
-          >
-            <img src={''} alt="" className="upload__img" />
+        {!loading ? (
+          <div>
+            <Link to="/">
+              <button className="cancel__form">X</button>
+            </Link>
+            <div className="form__heading">
+              <h1 id="greeting__msg">Hi There &#128075;</h1>
+              <p id="form__msg">
+                Feel free to share your ideas and the community will help you to turn it into a
+                product.
+              </p>
+            </div>
+
+            <form className="form__container">
+              <label htmlFor="name" className="input__lables">
+                Enter your full name:<span className="asteriks">*</span>
+              </label>
+              <input
+                className="input__field"
+                type="text"
+                id="name"
+                name="name"
+                value={formData.name}
+                onChange={handleChange}
+                required
+                autoComplete="off"
+              />
+              <label htmlFor="email" className="input__lables">
+                Enter email address:<span className="asteriks">*</span>
+              </label>
+              <input
+                className="input__field"
+                type="email"
+                id="email"
+                name="email"
+                value={formData.email}
+                onChange={handleChange}
+                required
+                autoComplete="off"
+              />
+              <label htmlFor="description" className="input__lables">
+                Brief Description of idea:<span className="asteriks">*</span>
+              </label>
+              <textarea
+                className="input__field text__area"
+                type="text"
+                id="description"
+                name="description"
+                value={formData.description}
+                onChange={handleChange}
+                required
+                autoComplete="off"
+              />
+              <label htmlFor="dropzone" className="input__lables">
+                Please share Graphical Description (If any)
+              </label>
+              <div
+                draggable
+                onDragOver={(e) => {
+                  e.preventDefault();
+                  e.stopPropagation();
+                }}
+                onDrop={(event) => onImageDropped(event)}
+                className="input__field drop__zone"
+                title="Drop Your Graphical Description Here"
+              >
+                <img src={''} alt="" className="upload__img" />
+              </div>
+              <label htmlFor="imageURL" className="input__lables">
+                Please share any Relavent Link ( Figma, etc )<span className="asteriks">*</span>
+              </label>
+              <input
+                className="input__field"
+                type="URL"
+                id="imageURL"
+                name="imageURL"
+                value={formData.imageURL}
+                onChange={handleChange}
+                autoComplete="off"
+              />
+              <button
+                title="Click to Submit Your Idea"
+                id="submit__button"
+                type="submit"
+                onClick={submitIdea}
+              >
+                submit idea
+              </button>
+            </form>
           </div>
-          <label htmlFor="imageURL" className="input__lables">
-            Please share any Relavent Link ( Figma, etc )<span className="asteriks">*</span>
-          </label>
-          <input
-            className="input__field"
-            type="URL"
-            id="imageURL"
-            name="imageURL"
-            value={formData.imageURL}
-            onChange={handleChange}
-            autoComplete="off"
-          />
-          <button
-            title="Click to Submit Your Idea"
-            id="submit__button"
-            type="submit"
-            onClick={submitIdea}
-          >
-            submit idea
-          </button>
-        </form>
+        ) : (
+          //TODO: Idhar style kardo loading
+          <div> Loading </div>
+        )}
       </div>
     </>
   );
